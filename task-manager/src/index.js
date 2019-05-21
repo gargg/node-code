bc55bc1d386ad431d11e66ea282f379e0bed9827
Garima Garg <garima.garg@lloydsbanking.com>
2019-05-21 16:30:05 +0100

    Working with files

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -22,22 +22,48 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+
+// // ******* Upload files *********
+// const multer = require('multer')
+//
+// const upload = multer({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000,
+//     },
+//     fileFilter(req, file, cb) {
+//         if (!file.originalname.match(/\.(doc|docx)$/)) {
+//             return cb(new Error('File must be Word doc'))
+//         }
+//
+//         cb(undefined, true)
+//     }
+// })
+//
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//     res.send()
+// }, (error, req, res, next) => {
+//     res.status(400).send({ error:error.message})
+// })
+
+// ******* Populate data by id *********
+// const Task = require('./models/task')
+// const User = require('./models/user')
+//
+// const main = async () => {
+//     // const task = await Task.findById('5cdd4dc555f0689a42cada77')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
+//     // const user = await User.findById('5cdd4d002e0fc899d62de1ee')
+//     // await user.populate('tasks').execPopulate()
+//     // console.log(user.tasks)
+//
+// }
+//
+// main()
+
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5cdd4dc555f0689a42cada77')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-    // const user = await User.findById('5cdd4d002e0fc899d62de1ee')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-
-}
-
-main()
 
